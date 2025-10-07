b827c3fb05229dfb12be223506fa2f7838d42e56
arsjobus <arsjobus@outlook.com>
2025-10-07 22:00:08 +0700

    refactor

--- src/TNTBlock.cpp
+++ src/TNTBlock.cpp
@@ -1,63 +1,40 @@
 #include "TNTBlock.h"
 
-TNTBlock::TNTBlock( void )
-{
+TNTBlock::TNTBlock( void ) { this->loadDefaultSettings(); }
+TNTBlock::TNTBlock( float width, float height ) {
 	this->loadDefaultSettings();
-}
-
-TNTBlock::TNTBlock( float width, float height )
-{
-	this->loadDefaultSettings();
-
 	// Override default settings here:
 	this->setSize( sf::Vector2f( width, height ) );
-
 	this->setOrigin(sf::Vector2f(this->getGlobalBounds().size.x / 2.f, this->getGlobalBounds().size.y / 2.f ));
 }
-
 /**
 * Destructor
 */
-TNTBlock::~TNTBlock( void )
-{
-	this->unloadTextures();
-}
+TNTBlock::~TNTBlock( void ) { this->unloadTextures(); }
 
 /**
 * Updates members of this object.
 */
-void TNTBlock::update(std::vector<Block *> block)
-{
+void TNTBlock::update(std::vector<Block *> block) {
 	Block::update(block);
-
 	// Override Block updates here:
-	if ( !this->getActive() && !this->getHasExploded() )
-	{
+	if ( !this->getActive() && !this->getHasExploded() ) {
 		dt += deltaClock.restart();
-
 		if (explosion == NULL) {
 			explosion = new Explosion( this->getExplosiveRadius(), this->getPosition().x, this->getPosition().y );
 
 			explosion->playSound( 0 );
 		}
-
 		if (!explosion->getActive() || 
-			dt.asMilliseconds() >= sf::milliseconds(250).asMilliseconds())
-		{
+			dt.asMilliseconds() >= sf::milliseconds(250).asMilliseconds()) {
 			// Detect collision between explosion and other blocks.
-			for (int i = 0; i < block.size(); ++i)
-			{
-				if (block[i]->getActive() &&
-					explosion->detectCollisionWithBlocks(*block[i]))
-				{
+			for (int i = 0; i < block.size(); ++i) {
+				if (block[i]->getActive() && explosion->detectCollisionWithBlocks(*block[i])) {
 					block[i]->setHitPoints(block[i]->getHitPoints() - explosion->getDamage());
-
 					log.quickWrite(std::string( this->getLabel() + log.getSeparator() + "has dealt " + log.intToString(explosion->getDamage()) + " points of damage to " + block[i]->getLabel() + "." ));
 				}
 			}
-
 			dt = sf::seconds( 0 );
-
 			this->setHasExploded( true );
 		}
 	}
@@ -65,12 +42,9 @@ void TNTBlock::update(std::vector<Block *> block)
 
 void TNTBlock::render(Window &window) {
 	Block::render(window);
-
-	if (explosion != NULL)
-	{
+	if (explosion != NULL) {
 		if (explosion->getActive()) {
 			window.draw(*explosion);
-
 			explosion->nextFrame();
 		}
 	}
@@ -79,49 +53,21 @@ void TNTBlock::render(Window &window) {
 /**
 * Loads default settings.
 */
-void TNTBlock::loadDefaultSettings()
-{
+void TNTBlock::loadDefaultSettings() {
 	Block::loadDefaultSettings();
-
 	// Override Block default settings here:
 	this->deltaClock.restart();
-
 	this->dt = sf::seconds(0);
-
 	this->setDropChance( 0.f );
-
 	this->setTextureID( 6 );
-
 	this->setLabel( "TNTBlock" );
-
 	this->setHasExploded( false );
-
 	this->setExplosiveRadius( 50 );
-
 	explosion = NULL;
 }
 
-float TNTBlock::getExplosiveRadius()
-{
-	return this->explosiveRadius;
-}
-
-void TNTBlock::setExplosiveRadius(float explosiveRadius)
-{
-	this->explosiveRadius = explosiveRadius;
-}
-
-bool TNTBlock::getHasExploded()
-{
-	return this->hasExploded;
-}
-
-void TNTBlock::setHasExploded(bool hasExploded)
-{
-	this->hasExploded = hasExploded;
-}
-
-Explosion *TNTBlock::getExplosion()
-{
-	return this->explosion;
-}
+float TNTBlock::getExplosiveRadius() { return this->explosiveRadius; }
+void TNTBlock::setExplosiveRadius(float explosiveRadius) { this->explosiveRadius = explosiveRadius; }
+bool TNTBlock::getHasExploded() { return this->hasExploded; }
+void TNTBlock::setHasExploded(bool hasExploded) { this->hasExploded = hasExploded; }
+Explosion *TNTBlock::getExplosion() { return this->explosion; }
