a879c2c953cf6dfff8e138bc6d0c664c694fb537
arsjobus <arsjobus@outlook.com>
2025-10-07 21:32:31 +0700

    refactor

--- src/DamageIncrease.cpp
+++ src/DamageIncrease.cpp
@@ -1,34 +1,21 @@
 #include "DamageIncrease.h"
 
-DamageIncrease::DamageIncrease(void)
-{
-	loadDefaultSettings();
-}
+DamageIncrease::DamageIncrease(void) { loadDefaultSettings(); }
+DamageIncrease::~DamageIncrease(void) { }
 
-DamageIncrease::~DamageIncrease(void)
-{
-}
-
-void DamageIncrease::update()
-{
+void DamageIncrease::update() {
 	PowerUp::update();
-
 	// Override PowerUp updates here:
 }
 
-void DamageIncrease::loadDefaultSettings()
-{
+void DamageIncrease::loadDefaultSettings() {
 	PowerUp::loadDefaultSettings();
-
 	// Override PowerUp default settings here:
 	this->setPowerID( PowerUp::TypeID::IncreasedDamage );
-
 	this->setTexture( &resource.getPowerupTexture( 0 ) );
 }
 
-void DamageIncrease::render(Window &window)
-{
+void DamageIncrease::render(Window &window) {
 	PowerUp::render(window);
-
 	// Make additional renders here:
 }
