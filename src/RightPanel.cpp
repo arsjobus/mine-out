045b79d3816e4c8d463fc883af3c3f5dec39bc79
arsjobus <arsjobus@outlook.com>
2025-10-07 21:56:05 +0700

    refactor

--- src/RightPanel.cpp
+++ src/RightPanel.cpp
@@ -1,34 +1,19 @@
 #include "RightPanel.h"
 
-RightPanel::RightPanel(void)
-{
+RightPanel::RightPanel(void) { this->loadDefaultSettings(); }
+RightPanel::RightPanel(int newWidth, int newHeight) {
 	this->loadDefaultSettings();
-}
-
-RightPanel::RightPanel(int newWidth, int newHeight)
-{
-	this->loadDefaultSettings();
-
 	iWidth = newWidth;
 	iHeight = newHeight;
-
 	// Override default settings here:
 	this->setSize(sf::Vector2f(iWidth, iHeight));
-
 	this->setOrigin(sf::Vector2f(getGlobalBounds().size.x / 2, getGlobalBounds().size.y / 2));
 }
+RightPanel::~RightPanel(void) { }
 
-RightPanel::~RightPanel(void)
-{
-}
-
-void RightPanel::loadDefaultSettings()
-{
+void RightPanel::loadDefaultSettings() {
 	this->setSize(sf::Vector2f(100, 668));
-
     this->setFillColor(sf::Color::White);
-
 	this->setTexture(&resource.getLevelTexture( 2 ));
-
 	this->setOrigin(sf::Vector2f(getGlobalBounds().size.x / 2, getGlobalBounds().size.y / 2));
 }
--- src/StoneBlock.cpp
+++ src/StoneBlock.cpp
@@ -1,70 +1,46 @@
 #include "StoneBlock.h"
 
-StoneBlock::StoneBlock( void )
-{
+StoneBlock::StoneBlock( void ) { loadDefaultSettings(); }
+StoneBlock::StoneBlock( int newWidth, int newHeight ) {
 	loadDefaultSettings();
-}
-
-StoneBlock::StoneBlock( int newWidth, int newHeight )
-{
-	loadDefaultSettings();
-
 	// Override default settings here:
 	setSize( sf::Vector2f( newWidth, newHeight ) );
-
 	setOrigin( sf::Vector2f(getGlobalBounds().size.x / 2.f, getGlobalBounds().size.y / 2.f) );
 }
-
 /**
 * Destructor
 */
-StoneBlock::~StoneBlock(void)
-{
-	unloadTextures();
-}
+StoneBlock::~StoneBlock(void) { unloadTextures(); }
 
-void StoneBlock::render(Window &window) {
-	Block::render(window);
-}
+void StoneBlock::render(Window &window) { Block::render(window); }
 
 /**
 * Updates members of this object.
 */
-void StoneBlock::update(std::vector<Block *> block)
-{
+void StoneBlock::update(std::vector<Block *> block) {
 	// Change textures before allowing Block to update.
 	if (this->getActive() && 
 		this->getTextureID() != 2 &&
 		this->getHitPoints() == 2)
 			this->setTextureID(2);
-
 	else if (getActive() && 
 		this->getTextureID() != 3 &&
 		this->getHitPoints() == 1)
 			this->setTextureID(3);
-
 	Block::update(block);
-
 	// Make additional updates specific to this object here:
 }
 
 /**
 * Loads default settings.
 */
-void StoneBlock::loadDefaultSettings()
-{
+void StoneBlock::loadDefaultSettings() {
 	Block::loadDefaultSettings();
-
 	// Override Block default settings here:
 	setDropChance( 20.f );
-
 	setHitPoints( 3 );
-
 	setFillColor( sf::Color( 255, 255, 255 ) );
-
 	setLabel( "StoneBlock" );
-
 	setTextureID( 1 );
-
 	setTexture( &resource.getBlockTexture( getTextureID() ) );
 }
