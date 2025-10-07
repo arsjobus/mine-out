3b9e638d200682e6d74a64d38385065e8f7ff662
arsjobus <arsjobus@outlook.com>
2025-10-07 21:41:57 +0700

    refactor

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,32 +1,12 @@
 #include "GameState.h"
 
-sf::String &GameState::getFontDirectoryName()
-{
-	return strFontDirectoryName;
-}
+sf::String &GameState::getFontDirectoryName() { return strFontDirectoryName; }
+void GameState::setFontDirectoryName(const char *newDirectoryName) { strFontDirectoryName = newDirectoryName; }
+sf::Font &GameState::getPrimaryFont() { return fntPrimaryFont; }
+sf::Color &GameState::getDefaultTextColor() { return defaultTextColor; }
+void GameState::setDefaultTextColor(sf::Color newColor) { defaultTextColor = newColor; }
 
-void GameState::setFontDirectoryName(const char *newDirectoryName)
-{
-	strFontDirectoryName = newDirectoryName;
-}
-
-sf::Font &GameState::getPrimaryFont()
-{
-	return fntPrimaryFont;
-}
-
-sf::Color &GameState::getDefaultTextColor()
-{
-	return defaultTextColor;
-}
-
-void GameState::setDefaultTextColor(sf::Color newColor)
-{
-	defaultTextColor = newColor;
-}
-
-void GameState::setPrimaryFont(const std::string &relativePathToFile)
-{
+void GameState::setPrimaryFont(const std::string &relativePathToFile) {
 	if (!fntPrimaryFont.openFromFile(relativePathToFile))
 		log.quickWrite(LOG_WARNING, std::string(getCurrentModeName() + log.getSeparator() + "Could not load primary font."));
 	else {
@@ -34,50 +14,19 @@ void GameState::setPrimaryFont(const std::string &relativePathToFile)
 	}
 }
 
-sf::String &GameState::getCurrentModeName()
-{
-	return strCurrentModeName;
-}
+sf::String &GameState::getCurrentModeName() { return strCurrentModeName; }
+void GameState::setCurrentModeName(const char *newModeName) { strCurrentModeName = newModeName; }
+sf::String &GameState::getPrimaryFontName() { return strPrimaryFontName; }
+void GameState::setPrimaryFontName(const char *newFontName) { strPrimaryFontName = newFontName; }
+sf::Font &GameState::getSecondaryFont() { return fntSecondaryFont; }
 
-void GameState::setCurrentModeName(const char *newModeName)
-{
-	strCurrentModeName = newModeName;
-}
-
-sf::String &GameState::getPrimaryFontName()
-{
-	return strPrimaryFontName;
-}
-
-void GameState::setPrimaryFontName(const char *newFontName)
-{
-	strPrimaryFontName = newFontName;
-}
-
-sf::Font &GameState::getSecondaryFont()
-{
-	return fntSecondaryFont;
-}
-
-void GameState::setSecondaryFont(const std::string &relativePathToFile)
-{
+void GameState::setSecondaryFont(const std::string &relativePathToFile) {
 	if (!fntSecondaryFont.openFromFile(relativePathToFile))
 		log.quickWrite(LOG_WARNING, "Could not load secondary font.");
 	else
 		log.quickWrite(LOG_SUCCESS, "Secondary font was loaded!");
 }
 
-sf::String &GameState::getSecondaryFontName() 
-{
-	return strSecondaryFontName;
-}
-
-void GameState::setSecondaryFontName(const char *newFontName)
-{
-	strSecondaryFontName = newFontName;
-}
-
-sf::RectangleShape &GameState::getRefToBackground()
-{
-	return rectBackground;
-}
+sf::String &GameState::getSecondaryFontName() { return strSecondaryFontName; }
+void GameState::setSecondaryFontName(const char *newFontName) { strSecondaryFontName = newFontName; }
+sf::RectangleShape &GameState::getRefToBackground() { return rectBackground; }
