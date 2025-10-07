723f8221cb30db37639d49e5d3a40198d795d36a
arsjobus <arsjobus@outlook.com>
2025-10-07 22:01:34 +0700

    refactor

--- src/TopPanel.cpp
+++ src/TopPanel.cpp
@@ -1,34 +1,19 @@
 #include "TopPanel.h"
 
-TopPanel::TopPanel(void)
-{
+TopPanel::TopPanel(void) { this->loadDefaultSettings(); }
+TopPanel::TopPanel(int newWidth, int newHeight) {
 	this->loadDefaultSettings();
-}
-
-TopPanel::TopPanel(int newWidth, int newHeight)
-{
-	this->loadDefaultSettings();
-	
 	// Override default settings here:
 	iWidth = newWidth;
 	iHeight = newHeight;
-
 	this->setSize(sf::Vector2f(iWidth, iHeight));
-
 	this->setOrigin(sf::Vector2f(getGlobalBounds().size.x / 2, getGlobalBounds().size.y / 2));
 }
+TopPanel::~TopPanel(void) { }
 
-TopPanel::~TopPanel(void)
-{
-}
-
-void TopPanel::loadDefaultSettings()
-{
+void TopPanel::loadDefaultSettings() {
 	this->setSize(sf::Vector2f(1024, 768));
-
     this->setFillColor(sf::Color::White);
-
 	this->setTexture(&resource.getLevelTexture( 0 ));
-
 	this->setOrigin(sf::Vector2f(getGlobalBounds().size.x / 2, getGlobalBounds().size.y / 2));
 }
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,42 +1,23 @@
 #include "Window.h"
 
-Window::Window(void)
-{
+Window::Window(void) {
 	bFullScreen = false;
-
 	// The minimum possible screen resolution
 	uMinimumScreenResolution.x = 600;
 	uMinimumScreenResolution.y = 400;
-
 	// The default/fallback screen resolution
 	uDefaultScreenResolution.x = 800;
 	uDefaultScreenResolution.y = 600;
-
 	// The default window title
 	defaultWindowTitle = "Breakout";
-
 	// Load settings from settings.ini file into qualifying object members
 	loadFromConfig();
 }
+Window::~Window(void) { }
 
-Window::~Window(void)
-{
-}
-
-bool Window::getFullScreen()
-{
-	return bFullScreen;
-}
-
-sf::Vector2u &Window::getScreenResolution()
-{
-	return uScreenResolution;
-}
-
-sf::String &Window::getDefaultWindowTitle()
-{
-	return defaultWindowTitle;
-}
+bool Window::getFullScreen() { return bFullScreen; }
+sf::Vector2u &Window::getScreenResolution() { return uScreenResolution; }
+sf::String &Window::getDefaultWindowTitle() { return defaultWindowTitle; }
 
 /**
 * Load settings from settings.ini file into qualifying object members
@@ -46,13 +27,11 @@ void Window::loadFromConfig() {
 	bFullScreen = config.getSettingAsBool("bFullScreen", "settings.ini");
 	uScreenResolution.x = config.getSettingAsUInt("uWindowWidth", "settings.ini");
 	uScreenResolution.y = config.getSettingAsUInt("uWindowHeight", "settings.ini");
-
 	// Ensure that the window is not set too small in the ini file
 	if (uScreenResolution.x < uMinimumScreenResolution.x || uScreenResolution.y < uMinimumScreenResolution.y) {
 		uScreenResolution.x = uDefaultScreenResolution.x;
 		uScreenResolution.y = uDefaultScreenResolution.y;
 	}
-
 	// Log and output information to console
 	if (uScreenResolution.x == 800 && uScreenResolution.y == 600)
 		log.quickWrite(LOG_INFO, std::string("Setting window resolution to 800 x 600"));
