7ee3a21407ec289553f6b0c76c01f8f50c0b4454
arsjobus <arsjobus@outlook.com>
2025-10-08 07:07:43 +0700

    refactor

--- src/Log.cpp
+++ src/Log.cpp
@@ -2,37 +2,26 @@
 
 std::string Log::filename = "log.txt";
 
-Log::Log(void)
-{
+Log::Log(void) {
 	infoLabel = "[INFO] ";
 	warningLabel = "[WARN] ";
 	errorLabel = "[ERRO] ";
 	successLabel = "[SUCC] ";
-
 	setSeparator(": ");
-
 	setFilename("log.txt");
-
 	// Clear log
 	clear();
 }
 
-Log::~Log(void)
-{
-	logfileWriteable.close();
-}
+Log::~Log(void) { logfileWriteable.close(); }
 
-void Log::clear() 
-{
+void Log::clear() {
 	// Clear the contents of the log file
-
 	logfileWriteable.open(filename, std::ofstream::out | std::ofstream::trunc);
-
 	logfileWriteable.close();
 }
 
-bool Log::eventIsAllowedToConsoleOutput(int logtype)
-{
+bool Log::eventIsAllowedToConsoleOutput(int logtype) {
 	if (DEBUG_OUTPUT_ENABLED &&
 		logtype == logtype::LOG_INFO && DEBUG_OUTPUT_INFO ||
 		logtype == logtype::LOG_SUCCESS && DEBUG_OUTPUT_SUCCESS ||
@@ -43,8 +32,7 @@ bool Log::eventIsAllowedToConsoleOutput(int logtype)
 		return false;
 }
 
-bool Log::eventIsAllowedToLog(int logtype)
-{
+bool Log::eventIsAllowedToLog(int logtype) {
 	if (DEBUG_LOG_ENABLED &&
 		logtype == logtype::LOG_INFO && DEBUG_LOG_INFO ||
 		logtype == logtype::LOG_SUCCESS && DEBUG_LOG_SUCCESS ||
@@ -55,14 +43,10 @@ bool Log::eventIsAllowedToLog(int logtype)
 		return false;
 }
 
-bool Log::exists()
-{
+bool Log::exists() {
 	std::string error_message = filename;
-
 	error_message.append(" could not be found!");
-
 	logfileReadable.open(filename.c_str());
-
 	if (logfileReadable.good()) {
         logfileReadable.close();
         return true;
@@ -74,14 +58,10 @@ bool Log::exists()
     }   
 }
 
-bool Log::exists(const char* customFilename)
-{
+bool Log::exists(const char* customFilename) {
 	std::string error_message = customFilename;
-
 	error_message.append(" could not be found!");
-
 	logfileReadable.open(customFilename);
-
 	if (logfileReadable.good()) {
         logfileReadable.close();
         return true;
@@ -93,169 +73,119 @@ bool Log::exists(const char* customFilename)
     }   
 }
 
-std::string Log::floatToString(float fltNumber)
-{
+std::string Log::floatToString(float fltNumber) {
 	std::stringstream ss;
-
 	ss << fltNumber;
-
 	std::string strNumber = ss.str();
-
 	return strNumber;
 }
 
-std::string Log::intToString(int intNumber)
-{
+std::string Log::intToString(int intNumber) {
 	std::stringstream ss;
-
 	ss << intNumber;
-
 	std::string strNumber = ss.str();
-
 	return strNumber;
 }
 
-void Log::open()
-{
+void Log::open() {
 	if (exists())
 		logfileWriteable.open(filename.c_str(), std::fstream::out | std::fstream::app);
 }
 
-void Log::open(const char* customFilename)
-{
+void Log::open(const char* customFilename) {
 	if (exists(customFilename))
 		logfileWriteable.open(customFilename, std::fstream::out | std::fstream::app);
 }
 
-const char *Log::outputLogType(int logtype)
-{
-	switch (logtype)
-	{
-	case LOG_INFO:
-		return infoLabel.c_str();
-		break;
-	case LOG_WARNING:
-		return warningLabel.c_str();
-		break;
-	case LOG_ERROR:
-		return errorLabel.c_str();
-		break;
-	case LOG_SUCCESS:
-		return successLabel.c_str();
-		break;
+const char *Log::outputLogType(int logtype) {
+	switch (logtype) {
+		case LOG_INFO:
+			return infoLabel.c_str();
+			break;
+		case LOG_WARNING:
+			return warningLabel.c_str();
+			break;
+		case LOG_ERROR:
+			return errorLabel.c_str();
+			break;
+		case LOG_SUCCESS:
+			return successLabel.c_str();
+			break;
 	}
 }
 
-void Log::quickWrite(const char* textToWrite)
-{
-	if (eventIsAllowedToLog(logtype::LOG_INFO) || eventIsAllowedToConsoleOutput(logtype::LOG_INFO))
-	{
+void Log::quickWrite(const char* textToWrite) {
+	if (eventIsAllowedToLog(logtype::LOG_INFO) || eventIsAllowedToConsoleOutput(logtype::LOG_INFO)) {
 		open();
-
 		if (exists()) {
 			write(logtype::LOG_INFO, textToWrite);
 			logfileWriteable.close();
 		}
-		else
-			throw "failed to write to log file.";
+		else throw "failed to write to log file.";
 	}
 }
 
-void Log::quickWrite(std::string textToWrite)
-{
-	if (eventIsAllowedToLog(logtype::LOG_INFO) || eventIsAllowedToConsoleOutput(logtype::LOG_INFO))
-	{
+void Log::quickWrite(std::string textToWrite) {
+	if (eventIsAllowedToLog(logtype::LOG_INFO) || eventIsAllowedToConsoleOutput(logtype::LOG_INFO)) {
 		open();
-
 		if (exists()) {
 			write(logtype::LOG_INFO, textToWrite);
 			logfileWriteable.close();
 		}
-		else
-			throw "failed to write to log file.";
+		else throw "failed to write to log file.";
 	}
 }
 
-void Log::quickWrite(int logtype, const char* textToWrite)
-{
-	if (eventIsAllowedToLog(logtype) || eventIsAllowedToConsoleOutput(logtype))
-	{
+void Log::quickWrite(int logtype, const char* textToWrite) {
+	if (eventIsAllowedToLog(logtype) || eventIsAllowedToConsoleOutput(logtype)) {
 		open();
-
 		if (exists()) {
 			write(logtype, textToWrite);
 			logfileWriteable.close();
 		}
-		else
-			throw "failed to write to log file.";
+		else throw "failed to write to log file.";
 	}
 }
 
-void Log::quickWrite(int logtype, const std::string &textToWrite)
-{
-	if (eventIsAllowedToLog(logtype) || eventIsAllowedToConsoleOutput(logtype))
-	{
+void Log::quickWrite(int logtype, const std::string &textToWrite) {
+	if (eventIsAllowedToLog(logtype) || eventIsAllowedToConsoleOutput(logtype)) {
 		open();
-
 		if (exists()) {
 			write(logtype, textToWrite);
 			logfileWriteable.close();
 		}
-		else
-			throw "failed to write to log file.";
+		else throw "failed to write to log file.";
 	}
 }
 
-void Log::setFilename(const char* newFilename)
-{
+void Log::setFilename(const char* newFilename) {
 	if (logfileWriteable.is_open())
 		logfileWriteable.close();
-
 	filename = newFilename;
 }
 
-void Log::setSeparator(const char *newSeparator)
-{
-	separator = newSeparator;
-}
+void Log::setSeparator(const char *newSeparator) { separator = newSeparator; }
 
-std::string &Log::getSeparator()
-{
-	return separator;
-}
+std::string &Log::getSeparator() { return separator; }
 
-void Log::write(int logtype, const char* textToWrite)
-{
-	if (logfileWriteable.is_open())
-	{
+void Log::write(int logtype, const char* textToWrite) {
+	if (logfileWriteable.is_open()) {
 		// Output to console
 		if (eventIsAllowedToConsoleOutput(logtype))
 			std::cout << outputLogType(logtype) << textToWrite << std::endl;
-
 		// Output to logfile
 		if (eventIsAllowedToLog(logtype))
 			logfileWriteable << outputLogType(logtype) << textToWrite << std::endl;
-	}
-	else
-	{
-		throw "failed to write to log file.";
-	}
+	} else throw "failed to write to log file.";
 }
 
-void Log::write(int logtype, const std::string &textToWrite)
-{
-	if (logfileWriteable.is_open())
-	{
+void Log::write(int logtype, const std::string &textToWrite) {
+	if (logfileWriteable.is_open()) {
 		// Output to console
 		if (eventIsAllowedToConsoleOutput(logtype))
 			std::cout << outputLogType(logtype) << textToWrite << std::endl;
-
 		// Output to logfile
 		if (eventIsAllowedToLog(logtype))
 			logfileWriteable << outputLogType(logtype) << textToWrite << std::endl;
-	}
-	else
-	{
-		throw "failed to write to log file.";
-	}
+	} else throw "failed to write to log file.";
 }
